04eb7a6c627a526767ab322e9a9fdd739bdfcfeb
gemcapalad <141731348+gemcapalad@users.noreply.github.com>
2024-08-05 15:09:46 +0800

    finally stopped the reload

--- login-script.js
+++ login-script.js
@@ -1,17 +1,30 @@
+document.addEventListener("DOMContentLoaded", function() {
+    formValidation();
+})
+
 function loginError() {
     document.getElementById("username").style.borderColor = '#ff3131'
     document.getElementById("password").style.borderColor = '#ff3131'
     document.getElementById("invalid").style.visibility = 'visible';
     document.getElementById("invalid").style.color = '#ff3131';
+    document.getElementById("username").blur();
+    document.getElementById("password").blur();
 }
 
-function stopReload() {
-    var form = document.getElementById("login-form");
-
-    function handleForm(event) {
+function formValidation() {
+    const form = document.getElementById("login-form");
+    form.addEventListener("submit", function(event) {
         event.preventDefault();
-    }
 
-    form.addEventListener('submit', handleForm)
+        const username = document.getElementById("username").value;
+        const password = document.getElementById("password").value;
+
+
+        if (username == "iamgmac" && password == "Taurus5723"){
+            alert("Login successful!")
+            location.reload();
+        } else {
+            loginError();
+        }
+    });
 }
-
