8ddb5f79c614e35ec8ca4eb1df70a7d96bae24d6
gemcapalad <141731348+gemcapalad@users.noreply.github.com>
2024-08-05 12:58:24 +0800

    success visibility failed enter button

--- login-script.js
+++ login-script.js
@@ -1,4 +1,17 @@
 function loginError() {
-    document.getElementById("usernamed").style.borderColor = '#ff3131'
+    document.getElementById("username").style.borderColor = '#ff3131'
     document.getElementById("password").style.borderColor = '#ff3131'
+    document.getElementById("invalid").style.visibility = 'visible';
+    document.getElementById("invalid").style.color = '#ff3131';
 }
+
+function stopReload() {
+    var form = document.getElementById("login-form");
+
+    function handleForm(event) {
+        event.preventDefault();
+    }
+
+    form.addEventListener('submit', handleForm)
+}
+
